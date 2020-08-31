e0060c23250af851930994971f386c24b89653f9
Gabor Ottlik <gaborottlik@Gabors-MacBook-Pro.local>
2020-08-31 19:53:18 +0200

    Get nested folders

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://api.index.hu/folders/folders/index.hu', {
+      headers: {
+        Authorization: '85IsCGestuf5B6WdaPC6QJl8v94tQuDjwEFqUCt4pp0EeqBJH'
+      }
+    }).then(response => response.json())
+    .then(data => {
+      console.log('-- data', data)
+      this.setState({ data: data.data })
+    })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
 }
 
 export default App;
