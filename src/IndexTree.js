a4d35a6c9c4265b66d7b66ed626d57a5822e18ac
Gabor Ottlik <gaborottlik@Gabors-MacBook-Pro.local>
2020-09-01 21:40:24 +0200

    Add search functionality to the tree

--- src/IndexTree.js
+++ src/IndexTree.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { Tree } from 'antd';
+import { Input } from 'antd'
 
 import 'antd/dist/antd.css';
 
+const { Search } = Input
+
 class IndexTree extends React.Component {
+
+  state = {
+    autoExpandParent: true,
+    expandedKeys: []
+  }
 
   constructTree (data) {
     const obj = data.map(node => {
@@ -19,20 +27,59 @@ class IndexTree extends React.Component {
   }
 
   onNodeSelected = (node, e) => {
-    console.log('-- node', e.node.path)
     this.props.setIframeUrl(e.node.path)
   }
 
+  onChange = (e) => {
+    const { data } = this.props
+    const { value } = e.target
+    
+    function findSearchMatch(tree) {
+      let matchedNode = []
+      debugger;
+
+      tree.forEach((item) => {
+        if (item.title.indexOf(value) != -1) {
+          matchedNode.push(item.key)
+        } else {
+          matchedNode = [
+            ...matchedNode,
+            ...findSearchMatch(item.children) 
+          ]
+        }
+      })
+
+      return matchedNode
+    }
+
+    const treeData = this.constructTree(data)
+    const searchedKey = findSearchMatch(treeData)
+
+    this.setState({ expandedKeys: searchedKey })
+  }
+
+  onExpand = expandedKeys => {
+    this.setState({
+      expandedKeys,
+      autoExpandParent: false,
+    });
+  };
+
   render() {
     const { data } = this.props
+    const { autoExpandParent, expandedKeys } = this.state
 
     const treeData = this.constructTree(data)
 
     return (
       <div className="index-tree">
+        <Search style={{ marginBottom: 8 }} placeholder="Search" onChange={this.onChange} />
         <Tree
-          treeData={treeData}
+          onExpand={this.onExpand}
           onSelect={this.onNodeSelected}
+          autoExpandParent={autoExpandParent}
+          treeData={treeData}
+          expandedKeys={expandedKeys}
         />
       </div>
     );
